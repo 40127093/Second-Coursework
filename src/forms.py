86abb27625b3194b10f8b71bfe188509fc8d5e88
40120793 <40127093@live.napier.ac.uk>
2016-11-20 16:33:17 +0000

    set up my login form and its validators

--- src/forms.py
+++ src/forms.py
@@ -44,4 +44,6 @@ class RegisterForm(Form):
        validators=[DataRequired()]
  )   
 
-
+class LoginForm(Form):
+     email = StringField('Email', validators=[DataRequired(), Email()])
+     password = PasswordField('Password', validators=[DataRequired()])
