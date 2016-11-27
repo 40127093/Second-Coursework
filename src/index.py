c88319083bad683ecc0266602f58f56b42257588
40120793 <40127093@live.napier.ac.uk>
2016-11-27 17:49:47 +0000

    Improved login and register pages, added links for a more user friendly navigation

--- src/index.py
+++ src/index.py
@@ -204,9 +204,6 @@ def logs(app):
 def not_found(error):
   return render_template('404.html'), 404
 
-@app.route('/test')
-def test():
-  return render_template('404test.html')
 
 # initialisation function
 
